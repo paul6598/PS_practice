ffe7f9e774eeec1e50ac99ed1f3daeb1200b083d
paul6598 <68279868+paul6598@users.noreply.github.com>
2024-08-29 00:24:00 +0900

    [Silver II] Title: 부당한 퍼즐, Time: 492 ms, Memory: 303164 KB -BaekjoonHub

--- 백준/Silver/15501. 부당한 퍼즐/부당한 퍼즐.py
+++ 백준/Silver/15501. 부당한 퍼즐/부당한 퍼즐.py
@@ -1,17 +1,11 @@
 n = int(input())
 arr1 = list(map(int,input().split()))
 arr2 = list(map(int,input().split()))
-arr3 = arr2[::-1]
 
 first = arr1[0]
 for i in range(n):
   if arr2[i] == first:
     index = i
-    break
-
-for i in range(n):
-  if arr3[i] == first:
-    index2 = i
     break
 
 a,b = True,True
@@ -20,14 +14,11 @@ for i in range(n):
   if arr1[i] != arr2[(i+index)%n]:
     a = False
     break
-#print()
+
 for i in range(n):
   #print(i,(i+index2)%n)
-  if arr1[i] != arr3[(i+index2)%n]:
+  if arr1[i] != arr2[(index-i)%n]:
     b = False
     break
 
-if a or b:
-  print("good puzzle")
-else:
-  print("bad puzzle")
+print("good puzzle" if a or b else "bad puzzle")
