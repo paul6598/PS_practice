00206d0bf04ed8bcadb535791b727022cdab0e23
paul6598 <68279868+paul6598@users.noreply.github.com>
2024-09-01 18:53:11 +0900

    [Silver I] Title: 걷다보니 신천역 삼 (Large), Time: 88 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/14651. 걷다보니 신천역 삼 （Large）/걷다보니 신천역 삼 （Large）.py
+++ 백준/Silver/14651. 걷다보니 신천역 삼 （Large）/걷다보니 신천역 삼 （Large）.py
@@ -3,9 +3,9 @@ n = int(input())
 if n ==1:
   print(0)
 else:
-  dp = [2]
+  dp = 2
   for _ in range(n-2):
-    dp.append((dp[-1]*3)%1000000009)
-
-  print(dp[-1])
+    dp *= 3
+    dp %= 1000000009
+  print(dp)
   
