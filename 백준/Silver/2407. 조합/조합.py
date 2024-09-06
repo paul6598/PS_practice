3a5b0897ba71fb3eb53a7c0d84bfc9214f43dd58
paul6598 <68279868+paul6598@users.noreply.github.com>
2024-09-06 14:36:47 +0900

    [Silver III] Title: 조합, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.py
+++ 백준/Silver/2407. 조합/조합.py
@@ -1,10 +1,10 @@
-def fac(n):
+def fac(n,m=1):
   ret = 1
-  for i in range(1,n+1):
+  for i in range(m+1,n+1):
     ret *= i
   return ret
 
 def sol(n,m):
-  return fac(n)//(fac(m)*fac(n-m))
+  return fac(n,m)//fac(n-m)
 n,m = map(int,input().split())
 print(sol(n,m))
