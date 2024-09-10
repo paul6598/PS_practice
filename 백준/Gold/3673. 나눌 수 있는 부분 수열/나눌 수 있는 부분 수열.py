6b9374834ae052296a0a3a10deec6b231129d694
paul6598 <68279868+paul6598@users.noreply.github.com>
2024-09-10 22:49:20 +0900

    [Gold III] Title: 나눌 수 있는 부분 수열, Time: 308 ms, Memory: 158328 KB -BaekjoonHub

--- 백준/Gold/3673. 나눌 수 있는 부분 수열/나눌 수 있는 부분 수열.py
+++ 백준/Gold/3673. 나눌 수 있는 부분 수열/나눌 수 있는 부분 수열.py
@@ -1,13 +1,15 @@
+import sys
+input = sys.stdin.readline
 from collections import defaultdict
 
 def sol(m,nums):
   ret = 0
   acc = [0]
-  for i in nums:
-    acc.append((acc[-1]+i)%m)
   dic = defaultdict(int)
-  for i in acc:
-    dic[i] += 1
+  for i in nums:
+    dic[acc[-1]] += 1
+    acc.append((acc[-1]+i)%m)
+  dic[acc[-1]] += 1
   for i in dic:
     ret += (dic[i]*(dic[i]-1))//2
   return ret
