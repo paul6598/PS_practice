2b8f11640b4eafc84cba193d197fbcbc77637d06
paul6598 <68279868+paul6598@users.noreply.github.com>
2024-09-11 16:54:54 +0900

    [Silver III] Title: 합 구하기, Time: 164 ms, Memory: 127140 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.py
+++ 백준/Silver/11441. 합 구하기/합 구하기.py
@@ -1,13 +1,10 @@
 import sys
 input = sys.stdin.readline
-
-n = int(input())
+_ = input()
 nums = list(map(int,input().split()))
 acc = [0]
 for i in nums:
   acc.append(acc[-1]+i)
-
-m = int(input())
-for _ in range(m):
+for _ in range(int(input())):
   i,j = map(int,input().split())
   print(acc[j]-acc[i-1])
