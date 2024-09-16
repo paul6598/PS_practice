d8120ad19d3107c1b6038e07577249336ceb46db
paul6598 <68279868+paul6598@users.noreply.github.com>
2024-09-17 01:14:10 +0900

    [Gold IV] Title: 비밀 모임, Time: 260 ms, Memory: 114676 KB -BaekjoonHub

---
+++ 백준/Gold/13424. 비밀 모임/비밀 모임.py
@@ -0,0 +1,50 @@
+"""
+플루이드 워셜
+"""
+
+
+t = int(input())
+for _ in range(t):
+  n,m = map(int,input().split())
+  graph = {i:{} for i in range(1,n+1)}
+  for _ in range(m):
+    a,b,t = map(int,input().split())
+    graph[a][b] = t
+    graph[b][a] = t
+  k = int(input())
+  friend = list(map(int,input().split()))
+
+  def floid(n,m,graph):
+    dist = [[float('inf')]*(n+1) for _ in range(n+1)]
+    for i in range(1,n+1):
+      dist[i][i] = 0
+
+    for i in graph:
+      for j in graph[i]:
+        dist[i][j] = graph[i][j]
+
+    for k in range(1,n+1):
+      for i in range(1,n+1):
+        for j in range(1,n+1):
+          dist[i][j] = min(dist[i][k]+dist[k][j],dist[i][j])
+
+    return dist
+
+  res = floid(n,m,graph)
+
+  mnv = float('inf')
+  mnk = 0
+
+  for i in range(1,n+1):
+    tmp = 0
+    for j in friend:
+      tmp += res[i][j]
+
+    if tmp < mnv:
+      mnv = tmp
+      mnk = i
+    elif tmp == mnv:
+      mnk = min(mnk,i)
+
+  print(mnk)
+
