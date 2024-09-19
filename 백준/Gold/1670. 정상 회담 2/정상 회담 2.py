780f9f4ae4556205d3fae34efdbf7f36a91c1d6a
paul6598 <68279868+paul6598@users.noreply.github.com>
2024-09-19 17:58:46 +0900

    [Gold III] Title: 정상 회담 2, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/1670. 정상 회담 2/정상 회담 2.py
+++ 백준/Gold/1670. 정상 회담 2/정상 회담 2.py
@@ -3,19 +3,13 @@ k = n//2
 dp = [1,1] #0,2,4,6
 for i in range(2,k+1):
   tmp = 0
-  if i%2 == 0:
-    for j in range(i//2):
-      tmp += dp[j]*dp[i-j-1]
-      tmp %= 987654321
-    tmp *= 2
+  for j in range(i//2):
+    tmp += dp[j]*dp[i-j-1]
     tmp %= 987654321
-  else:
-    for j in range(i//2):
-      tmp += dp[j]*dp[i-j-1]
-      tmp %= 987654321
-    tmp *= 2
+  tmp *= 2
+  if i%2 == 1:
     tmp += dp[i//2]*dp[i//2]
-    tmp %= 987654321
+  tmp %= 987654321
   dp.append(tmp)
 
 print(dp[-1])
