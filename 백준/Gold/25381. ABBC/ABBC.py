330ef785fe33d5daf0bf947e5beac9468fb0edc0
paul6598 <68279868+paul6598@users.noreply.github.com>
2024-09-24 13:23:51 +0900

    [Gold III] Title: ABBC, Time: 132 ms, Memory: 117860 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25381. ABBC/ABBC.py
+++ 백준/Gold/25381. ABBC/ABBC.py
@@ -1,7 +1,3 @@
-#한번 s를 탐색할때
-#a와 b의 인덱스를 q에 삽입하면서 c가 나오면 b에서 하나 뽑아서 res += 1
-#그 뒤 a와 b의 인덱스를 비교하면서 res에 추가
-
 from collections import deque
 
 a_ = deque([])
@@ -18,22 +14,18 @@ for i in range(len(s)):
     if len(b_) != 0:
       res += 1
       b_.popleft()
-        
 if len(b_)*len(a_) != 0:
   index = b_.popleft()
   a_index = a_.popleft()
   while True:
-    #print(a_index, index)
     if a_index < index:
       res += 1
-      #print(len(b_)*len(a_))
-      if len(b_)*len(a_) == 0:
+      if len(a_) == 0:
         break
       a_index = a_.popleft()
-      index = b_.popleft()
-    else:
-      if len(b_)*len(a_) == 0:
-        break
-      index = b_.popleft()
+
+    if len(b_) == 0:
+      break
+    index = b_.popleft()
 
 print(res)
