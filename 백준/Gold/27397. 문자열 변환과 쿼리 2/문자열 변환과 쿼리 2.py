44de3468f24631b0896832e29aab901278415430
paul6598 <68279868+paul6598@users.noreply.github.com>
2025-02-23 13:17:01 +0900

    [Gold IV] Title: 문자열 변환과 쿼리 2, Time: 7704 ms, Memory: 114676 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/27397. 문자열 변환과 쿼리 2/문자열 변환과 쿼리 2.py
+++ 백준/Gold/27397. 문자열 변환과 쿼리 2/문자열 변환과 쿼리 2.py
@@ -1,21 +1,27 @@
-arr, n = input().split()
-n = int(n)
+dic = {}
+for x in range(65,91):
+  dic[chr(x)] = chr(x)
+for x in range(97,123):
+  dic[chr(x)] = chr(x)
 
-def two(arr):
-  ret = 1
+def sol(dic, arr):
+  res = 1
   tmp = 1
   for i in range(len(arr)-1):
-    if arr[i] == arr[i+1]:
+    if dic[arr[i]] == dic[arr[i+1]]:
       tmp += 1
+      res = max(res, tmp)
     else:
-      ret = max(ret,tmp)
       tmp = 1
-  ret = max(ret,tmp)
-  return ret
+  return res
 
-for _ in range(n):
+arr, n = input().split()
+
+for _ in range(int(n)):
   tmp = list(input().split())
-  if len(tmp) == 1:
-    print(two(arr))
+  if tmp[0] == '2':
+    print(sol(dic, arr))
   else:
-    arr = arr.replace(tmp[1],tmp[2])
+    for i in dic:
+      if dic[i] == tmp[1]:
+        dic[i] = tmp[2]
